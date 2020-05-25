f3c1f014db76a1c06c648bb10c050b7185f36177
Leo Fabre <fabre.leo@gmail.com>
2020-05-25 19:12:17 +0200

    Testing star rain

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
+stars = [];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 function setup() {
     createCanvas(400, 400);
+    for (var i = 0; i < 32; i++) {
+        stars.push(new Star(randint(max_size)));
+    }
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+    background("#001536");
+    for (var i = 0; i < stars.length; i++) {
+        stars[i].show();
     }
-    ellipse(mouseX, mouseY, 80, 80);
 }
